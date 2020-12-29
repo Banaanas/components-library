bd58b02aa1adfc832917fdb6d04658d2446bac13
Cyril <cyrildeveloppeur@gmail.com>
2020-12-29 18:03:05 +0100

    Refactor

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -53,8 +53,9 @@ const Header = () => {
   // Throttle the callback function to optimize  component performances by
   // preventing too many useless renders
   const onWindowScrollHandler = useThrottledFn(() => {
-    // If SSR, Return
+    // If SSR, Return (because Window is NOT defined on the Node.js Server)
     if (typeof window === "undefined") return;
+
     if (window.pageYOffset > 1) {
       // Header Drop Shadow when Scroll - No Drop Shadow on Top
       setBoxShadow(`0 0 100px 0 ${theme.colors.primary.light}`);
--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,11 +1,12 @@
 import { useRef, useState } from "react";
+import { useThrottledFn, useWindowResize } from "beautiful-react-hooks";
 import styled from "@emotion/styled";
 import FocusLock from "react-focus-lock";
 import useOnClickOutside from "../../custom-hooks/useOnClickOutside";
 import Burger from "./Burger";
 import NavBar from "./NavBar";
 
-const StyledRefDiv = styled.div`
+const StyledDiv = styled.div`
   @media (min-width: 710px) {
     display: none;
   }
@@ -13,15 +14,33 @@ const StyledRefDiv = styled.div`
 
 const SideMenu = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
   const DOMRef = useRef(null);
   const menuId = "main-menu";
 
   // Close Side SideMenu when click outside the Ref- custom Hook
   useOnClickOutside(DOMRef, () => setMenuOpen(false));
 
+  // useThrottledFn - CUSTOM HOOK
+  // Throttle the callback function to optimize  component performances by
+  // preventing too many useless renders
+  const onWindowResizeHandler = useThrottledFn(() => {
+    // If SSR, Return (because Window is NOT defined on the Node.js Server)
+    if (typeof window === "undefined") return;
+
+    // Close SideMenu if it was Open
+    setWidth(window.innerWidth);
+    if (width > 710) setMenuOpen(false);
+  }, 200);
+
+  // useWindowResize - CUSTOM HOOK
+  // Resize Event Listener (Add AND Cleanup Event)
+  useWindowResize(onWindowResizeHandler);
+
   return (
     <>
-      <StyledRefDiv ref={DOMRef}>
+      <StyledDiv ref={DOMRef}>
         <FocusLock disabled={!isMenuOpen}>
           <Burger
             openMenu={isMenuOpen}
@@ -34,7 +53,7 @@ const SideMenu = () => {
             menuID={menuId}
           />
         </FocusLock>
-      </StyledRefDiv>
+      </StyledDiv>
     </>
   );
 };
